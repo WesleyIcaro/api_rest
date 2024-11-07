75b1b906aaf9730797b354e8b0df36e577849a50
Wesley Icaro Lima dos Santos <wesleyicaro4@gmail.com>
2024-11-07 17:39:54 -0300

    Beginner configuration of sequelize.

---
+++ src/config/database.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+require('dotenv').config();
+
+module.exports = {
+  dialect: 'mariadb',
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+  define: {
+
+  },
+  dialetOptions: {
+    timezone: 'America/Sao_Paulo'
+  },
+};
