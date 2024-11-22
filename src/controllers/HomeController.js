702e357066a7c274bf6fc3f1e6916d9e151adf6e
Wesley Icaro Lima dos Santos <wesleyicaro4@gmail.com>
2024-11-22 18:40:14 -0300

    Iniciando a configuração do foto controller e rota.

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Jose',
-      sobrenome: 'Honorio',
-      email: 'Jose@gmail.com',
-      idade: 125,
-      peso: 90,
-      altura: 1.82,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
